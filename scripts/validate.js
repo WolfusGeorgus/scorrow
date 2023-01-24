bc84750ce0936bc6083e3a969bc3aaa5a5a1839e
Gabor Lajko <gabor.lajko@students.bs-linz2.ac.at>
2023-01-24 09:27:45 +0100

    Login js

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,7 +2,28 @@ import * as dbcall from './dbcall.js';
 
 window.onload = function () {
     validateReg();
+    validateLog();
 }
+function validateLog() {
+    document.getElementById("loginBTN").addEventListener("click", function (event) {
+        event.preventDefault(); // prevent the form from being submitted
+        var bName = document.getElementById("bName").value;
+        var psWord = document.getElementById("passWort").value;
+
+        if (bName === "" || psWord === "" ) {
+            alert("Bitte alle Felder ausfüllen!");
+        } else if (true == true) {
+            alert("Die Kennwörter stimmen nicht überein!");
+        } else if (psWord < 5) {
+            alert("Das Passwort muss mindestens 5 Zeichen lang sein!")
+        } else {
+            dbcall.CreateUser(vName, nName, bName, psWord);
+            location.href = "index.html";
+        }
+    });
+
+}
+
 
 function validateReg() {
     document.getElementById("erstellen").addEventListener("click", function (event) {
@@ -17,12 +38,15 @@ function validateReg() {
             alert("Bitte alle Felder ausfüllen!");
         } else if (psWord !== bPsWord) {
             alert("Die Kennwörter stimmen nicht überein!");
-        }else if(psWord < 10){
-            alert("Password")
+        }else if(psWord < 5){
+            alert("Das Passwort muss mindestens 5 Zeichen lang sein!")
         } else {
             dbcall.CreateUser(vName, nName, bName, psWord);
             alert("Die Registrierung wurde abgeschlossen.");
             location.href = "login.html";
         }
     });
+
+
+
 }
