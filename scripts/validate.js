a1d91c5de8734da5cddd9ae879cfce4c3345c429
Gabor Lajko <gabor.lajko@students.bs-linz2.ac.at>
2023-01-24 09:10:41 +0100

    Registation

---
+++ scripts/validate.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    validateReg();
+}
+
+function validateReg() {
+    document.getElementById("erstellen").addEventListener("click", function (event) {
+        event.preventDefault(); // prevent the form from being submitted
+        var bName = document.getElementById("bName").value;
+        var vName = document.getElementById("vName").value;
+        var nName = document.getElementById("nName").value;
+        var psWord = document.getElementById("psWord").value;
+        var bPsWord = document.getElementById("bPsWord").value;
+
+        if (bName === "" || vName === "" || nName === "" || psWord === "" || bPsWord === "") {
+            alert("Bitte alle Felder ausfüllen!");
+        } else if (psWord !== bPsWord) {
+            alert("Die Kennwörter stimmen nicht überein!");
+        }else if(psWord < 10){
+            alert("Password")
+        } else {
+            alert("Die Registrierung wurde abgeschlossen.");
+            location.href = "login.html";
+        }
+    });
+}
